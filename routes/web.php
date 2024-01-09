9538e39bf5b42dc6f6e2a1af721c752b5ebda98f
aubry224 <aubry224@unicaen.fr>
2024-01-09 10:09:12 +0100

    feat : retrieve a specific dive

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\AcnMemberController;
+use App\Http\Controllers\AcnDivesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return AcnDivesController::getOneDive(2);
 });
