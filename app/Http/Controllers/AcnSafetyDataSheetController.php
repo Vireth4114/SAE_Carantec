e00aaddf20071670ecec9a9094718ee19c457189
MaitreValentino <valentinsnith@hotmail.com>
2024-01-11 10:21:10 +0100

    Add: Logo + ending safety sheet

--- app/Http/Controllers/AcnSafetyDataSheetController.php
+++ app/Http/Controllers/AcnSafetyDataSheetController.php
@@ -2,12 +2,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\web\AcnDives;
-use App\Models\web\AcnBoat;
+use App\Models\web\AcnGroups;
 use Illuminate\Support\Facades\DB;
 
 class AcnSafetyDataSheetController extends Controller {
     static public function getAll() {
         $dives = AcnDives::all();
+        $palanquing = AcnGroups::all();
 
         $pilote = DB::table('ACN_MEMBER')
         ->select('MEM_NAME', 'MEM_SURNAME')
@@ -26,6 +27,7 @@ class AcnSafetyDataSheetController extends Controller {
 
         $registered = DB::table('ACN_REGISTERED')
         ->selectRaw("NUM_GROUPS, count(*) as REGISTERED")
+        ->where('NUM_GROUPS', '=', 2)
         ->groupBy('NUM_GROUPS')
         ->get();
 
@@ -44,9 +46,17 @@ class AcnSafetyDataSheetController extends Controller {
         ->where('PER_NUM_PERIOD', '=', $dives[0]->DIV_NUM_PERIOD)
         ->get();
 
+        $members = DB::table('ACN_REGISTERED')
+        ->select('MEM_NAME', 'MEM_SURNAME')
+        ->join('ACN_MEMBER', 'ACN_MEMBER.MEM_NUM_MEMBER', '=', 'ACN_REGISTERED.NUM_MEMBER')
+        ->join('ACN_GROUPS', 'ACN_GROUPS.GRP_NUM_GROUPS', '=', 'ACN_REGISTERED.NUM_GROUPS')
+        ->where('ACN_REGISTERED.NUM_GROUPS', '=', 2)
+        ->get();
+
         return view('safetyDataSheet', ["dives" => $dives[0], "pilote" => $pilote[0],
         "secure" => $secure[0], "lead" => $lead[0], "registered" => $registered[0],
-        "boat" => $boat[0], "site" => $site[0], "period" => $period[0]]);
+        "boat" => $boat[0], "site" => $site[0], "period" => $period[0], "palanquing" => $palanquing[0],
+        "members" => $members]);
     }
 }
 
--- resources/views/safetyDataSheet.blade.php
+++ resources/views/safetyDataSheet.blade.php
@@ -1,7 +1,6 @@
 @extends('template')
 
 @section('content')
-<h1>Fiche de sécurité</h1>
     @php
         setlocale(LC_TIME, ['fr', 'fra', 'fr_FR']);
         $timestamp = strtotime(date_Format($dives->DIV_DATE, 'l j F Y'));
@@ -10,48 +9,98 @@
         $startTime = strftime('%H', $startTimestamp);
         $endTimestamp = strtotime($period->PER_END_TIME);
         $endTime = strftime('%H', $endTimestamp);
+        $immertionTimestamp = strtotime($palanquing->GRP_TIME_OF_IMMERSION);
+        $immertionTime = strftime('%H:%M', $immertionTimestamp);
+        $emertionTimestamp = strtotime($palanquing->GRP_TIME_OF_EMERSION);
+        $emertionTime = strftime('%H:%M', $emertionTimestamp);
     @endphp
+<div id="safetySheet">
+    <table id="firstTable">
+        <thead>
+            <tr>
+                <th colspan="4">Fiche de sécurité</th>
+            </tr>
+        </thead>
+        <tr>
+            <td colspan="1">Date</td>
+            <td colspan="1">{{$date}} / {{$startTime.'h - '.$endTime.'h'}}</td>
+            <td id="imageTD" colspan="2" rowspan="4"><img src="/images/logoSafetySheet.png" alt="carantec nautism" width="600px"></td>
+        </tr>
+        <tr>
+            <td colspan="1">Directeur de plongée</td>
+            <td colspan="1">{{$lead->MEM_NAME.' '.$lead->MEM_SURNAME}}</td>
+        </tr>
+        <tr>
+            <td colspan="1">Site de plongée</td>
+            <td colspan="1">{{$site->SIT_NAME}}</td>
+        </tr>
+        <tr>
+            <td class="firstRow">Effectif</td>
+            <td class="secondRow">{{$registered->REGISTERED}}</td>
+        </tr>
+    </table>
 
-<table id="firstTable">
-    <thead>
+    <table id="secondTable">
+        <tr>
+            <td class="firstRow">Embarcation</td>
+            <td class="secondRow">{{$boat->BOA_NAME}}</td>
+        </tr>
+        <tr>
+            <td class="firstRow">Sécurité de surface</td>
+            <td class="secondRow">{{$secure->MEM_NAME.' '.$secure->MEM_SURNAME}}</td>
+        </tr>
+        <tr>
+            <td class="firstRow">Pilote</td>
+            <td class="secondRow">{{$pilote->MEM_NAME.' '.$pilote->MEM_SURNAME}}</td>
+        </tr>
+        <tr>
+            <td class="firstRow">Observation</td>
+            <td class="secondRow" rowspan="3"></td>
+        </tr>
+    </table>
 
-    </thead>
-    <tr>
-        <td class="firstRow">Date</td>
-        <td class="secondRow">{{$date}} / {{$startTime.'h - '.$endTime.'h'}}</td>
-    </tr>
-    <tr>
-        <td class="firstRow">Directeur de plongée</td>
-        <td class="secondRow">{{$lead->MEM_NAME.' '.$lead->MEM_SURNAME}}</td>
-    </tr>
-    <tr>
-        <td class="firstRow">Site de plongée</td>
-        <td class="secondRow">{{$site->SIT_NAME}}</td>
-    </tr>
-    <tr>
-        <td class="firstRow">Effectif</td>
-        <td class="secondRow">{{$registered->REGISTERED}}</td>
-    </tr>
-</table>
-
-<table id="secondTable">
-    <tr>
-        <td class="firstRow">Embarcation</td>
-        <td class="secondRow">{{$boat->BOA_NAME}}</td>
-    </tr>
-    <tr>
-        <td class="firstRow">Sécurité de surface</td>
-        <td class="secondRow">{{$secure->MEM_NAME.' '.$secure->MEM_SURNAME}}</td>
-    </tr>
-    <tr>
-        <td class="firstRow">Pilote</td>
-        <td class="secondRow">{{$pilote->MEM_NAME.' '.$pilote->MEM_SURNAME}}</td>
-    </tr>
-    <tr>
-        <td class="firstRow">Observation</td>
-        <td class="secondRow" rowspan="3"></td>
-    </tr>
-</table>
-
-
+    <table id="palanquing">
+        <thead>
+            <tr>
+                <th colspan="6">Palanquée n° {{$palanquing->GRP_NUM_GROUPS}}</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Heure de départ</td>
+                <td>{{$immertionTime}}</td>
+                <td>Heure de retour</td>
+                <td colspan="3">{{$emertionTime}}</td>
+            </tr>
+            <tr>
+                <td>Temps Prévu</td>
+                <td>{{$palanquing->GRP_EXPECTED_DURATION}} min</td>
+                <td>Profondeur Prévu</td>
+                <td colspan="3">{{$palanquing->GRP_EXPECTED_DEPTH}} m</td>
+            </tr>
+            <tr>
+                <td>Temps Réalisé</td>
+                <td>{{$palanquing->GRP_DIVING_TIME}} min</td>
+                <td>Profondeur Réalisé</td>
+                <td colspan="3">{{$palanquing->GRP_REACHED_DEPTH}} m</td>
+            </tr>
+            <tr>
+                <td id="nameSurname" colspan="3">Nom Prénom</td>
+                <td>Aptitudes</td>
+                <td>Formation vers</td>
+                <td>Fonction</td>
+            </tr>
+            @foreach ($members as $member)
+            <tr>
+                <td colspan="3">
+                    {{$member->MEM_SURNAME.' '.$member->MEM_NAME}}
+                </td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
