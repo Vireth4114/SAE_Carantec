2a6266ae96fc95db94740ccd50d66ca32afa98b6
Nicolle Raphaël <raphael.nicolle@etu.unicaen.fr>
2024-01-11 22:14:34 +0100

    fix : bootstrap is now local

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -8,7 +8,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{URL::asset('/css/app.css')}}" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href={{ asset('css/bootstrap.min.css') }}>
+
     <title>Document</title>
     <script src="https://kit.fontawesome.com/70f23b7858.js" crossorigin="anonymous"></script>
 </head>
